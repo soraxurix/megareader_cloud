522f60db84ba26f4a64df8c2ad405e536831741b
soraxurix <71204059+soraxurix@users.noreply.github.com>
2022-05-27 18:43:56 -0500

    prueba 1

---
+++ clases/conexion.php
@@ -0,0 +1,18 @@
+<?php 
+
+$servidor = "sql10.freemysqlhosting.net";
+$usuario = "sql10494751";
+$password = "6ju1W72rrT";
+$db = "sql10494751";
+
+$conexion = mysqli_connect($servidor,$usuario,$password,$db);
+
+/*if($conexion){
+	echo "Se conecto a la base de datos";
+}*/
+// Server: sql10.freemysqlhosting.net
+// Name: sql10494751
+// Username: sql10494751
+// Password: 6ju1W72rrT
+// Port number: 3306
+ ?>
