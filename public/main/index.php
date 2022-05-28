58f1c18675efb0ffd5c39b1d4e56b75ce35dcc6c
soraxurix <71204059+soraxurix@users.noreply.github.com>
2022-05-27 22:21:51 -0500

    agregando navbar

--- public/main/index.php
+++ public/main/index.php
@@ -35,7 +35,7 @@
 		  <div class="navbar-brand">
 		    <a class="navbar-item" href="#">
 		      <!-- <img src="https://raw.githubusercontent.com/soraxurix/megareader_cloud/main/logo%20horizontal.png"> -->
-			  <img src="https://raw.githubusercontent.com/soraxurix/megareader_cloud/main/logo%20horizontal.png" width="112" height="28">
+			  <img src="https://raw.githubusercontent.com/soraxurix/megareader_cloud/main/public/logo%20horizontal.png" width="112" height="28">
 		      <!-- <img src="https://raw.githubusercontent.com/soraxurix/GDLREADER/main/GDLREADER/app/src/main/res/drawable/logo.png">
 		      <span class="is-size-4">Megareader</span> -->
 		    </a>
